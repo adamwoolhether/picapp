640a36c483b67492f53fb78b72d5e91aa41a384d
adamwoolhether <adamwoolhether@gmail.com>
2021-01-23 13:19:58 +0800

    add faq page

--- main.go
+++ main.go
@@ -12,14 +12,21 @@ func home(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Welcome the the picapp site</h1>")
 }
 
+func faq(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<h1>FAQ Page</h1><p1><h2>Q 1:<br>Q 2:<br>Q 3:</h2>" +
+		"<a>For further inquiry, email me at: <a href=\"mailto:adamwoolhether@gmail.com\">adamwoolhether@gmail.com</a>")
+}
+
 func contact(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "<a>Contact me: <a href=\"mailto:adamwoolhether@gmail.com\">adamwoolhether@gmail.com</a></h1>")
+	fmt.Fprint(w, "<a>Contact me: <a href=\"mailto:adamwoolhether@gmail.com\">adamwoolhether@gmail.com</a>")
 }
 
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
+	r.HandleFunc("/faq", faq)
 	http.ListenAndServe("localhost:3000", r)
 }
