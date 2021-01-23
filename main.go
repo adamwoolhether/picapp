75d344c3402f19b286211a55e50fed1df4c22fdb
adamwoolhether <adamwoolhether@gmail.com>
2021-01-23 13:47:16 +0800

    add custom 404 page

--- main.go
+++ main.go
@@ -17,8 +17,20 @@ func contact(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<a>Contact me: <a href=\"mailto:adamwoolhether@gmail.com\">adamwoolhether@gmail.com</a></h1>")
 }
 
+func lost(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<h1>Sorry, this page doesn't exist</h1>")
+}
+
 func main() {
 	r := mux.NewRouter()
+
+	//Creating a custom 404 err response for Gorilla's mux:
+	// Because the backed of gorilla's mux NotFoundHandler directs to http.Handler
+	// you must implement the http.Handler interface and assign it to NotFoundHandler
+	var l http.Handler = http.HandlerFunc(lost)
+	r.NotFoundHandler = l
+
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
 	http.ListenAndServe("localhost:3000", r)
